b5aa852824150586c857d300754855bd34943f45
phoenix-1221 <lapislazuli.navy.blue@gmail.com>
2026-05-21 23:49:07 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js の例
+const firebaseConfig = {
+  apiKey: "AIzaSyAvjQFEYXfvPOeXglJe8qsA39MfjuJ6ccI",
+  authDomain: "shere-todo.firebaseapp.com",
+  projectId: "shere-todo",
+  storageBucket: "shere-todo.firebasestorage.app",
+  messagingSenderId: "342686107217",
+  appId: "1:342686107217:web:758eeecffbe16049d3da70",
+  measurementId: "G-448C8QDWTH"
+};
+
+// Firebase初期化
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
